cf6f00e971cf6a85dffa8081a91dc8be0fc224d8
Cloud Wu <cloudwu@gmail.com>
2019-03-10 16:04:17 +0800

    gen function id

--- temp.bgfx.shim.cpp
+++ temp.bgfx.shim.cpp
@@ -1,2 +1,19 @@
+struct FunctionId
+{
+	enum Enum
+	{
+		$cpp_functionid
+
+		Count
+	};
+};
+
+typedef enum bgfx_function_id
+{
+	$c99_functionid
+
+	BGFX_FUNCTION_ID_COUNT
+} bgfx_function_id_t;
+
 $cpp_interface
 $c99_interface
